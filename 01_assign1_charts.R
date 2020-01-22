949ba6043ffc37a6d827a3294deea791f9523f01
Nick Hoffman <Nick@Nicks-MacBook-Pro.local>
2020-01-22 16:27:46 -0500

    Finished writeup

--- 01_assign1_charts.R
+++ 01_assign1_charts.R
@@ -6,6 +6,7 @@ gamma2 <- sqrt(s)
 alphas <- c(-3 / 4, 1 / 4, 2)
 cols <- c("red", "forestgreen", "blue")
 
+png("assignment_writeups/plot_4.png")
 plot(s, gamma2, type = "l", xlim = c(-0.1, 1), ylim = c(-0.1, 1),
      ylab = "t", main = "Reaction functions")
 for (i in 1:length(alphas)){
@@ -13,3 +14,4 @@ for (i in 1:length(alphas)){
 }
 abline(h = 0)
 abline(v = 0)
+dev.off()
